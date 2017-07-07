35d894f3ec05672565b55af74f0eaf9d9db7c96f
Kilian Drayne <kilian.drayne@sla-ltd.com>
2017-07-07 10:37:07 +0100

    require presence of image, lat and lng values

--- app/models/mural.rb
+++ app/models/mural.rb
@@ -1,3 +1,6 @@
 class Mural < ApplicationRecord
 	mount_uploaders :images, ImageUploader
+	validates :images, :presence => {:message => "must be uploaded. "}
+	validates :lat, :long, :presence => {:message => "No marker placed on Map. "}
+
 end
